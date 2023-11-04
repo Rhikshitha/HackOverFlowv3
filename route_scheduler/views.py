27dc84e16df2a2f5087c4fa443a55fa929982079
sanjay0001 <75019244+sanjay0001@users.noreply.github.com>
2023-11-04 21:28:43 +0530

    Bug Fix

--- route_scheduler/views.py
+++ route_scheduler/views.py
@@ -1,17 +1,39 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 
 # Create your views here.
 def home(request):
-    return render(request,"index.html")
+    try:
+        token = request.COOKIES.get('jwt')
+        if not token:
+            print(token)
+            return redirect("login")
+
+            
+        return render(request,"index.html")
+    except Exception as e:
+        return HttpResponse(f"{e}")
 
 def passenger(request):
-    return render(request,"passangerMap.html")
+    try:
+        token = request.COOKIES.get('jwt')
+        if not token:
+            print(token)
+            return redirect("login")
+
+
+        return render(request,"passangerMap.html")
+    except Exception as e:
+        return HttpResponse(f"{e}")
+
 
 @csrf_exempt
 def set_route(request):
     data = json.loads(request.body.decode('utf-8'))
-    print(data)
+    route=data["route"]
+    source=data["source"]
+    destination=data["destination"]
+    print(source,destination)
     return JsonResponse({"data":"Done"})
