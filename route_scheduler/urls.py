8cac8866f5dfdbba4662573ebd77710aff0f8157
sanjay0001 <75019244+sanjay0001@users.noreply.github.com>
2023-11-04 20:38:36 +0530

    Bug Fix

--- route_scheduler/urls.py
+++ route_scheduler/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('',home,name="home"),
     path('passenger',passenger,name="passenger"),
+    path('set_route',set_route,name="set_route"),
 
 ]
--- route_scheduler/views.py
+++ route_scheduler/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def home(request):
     return render(request,"index.html")
 
 def passenger(request):
-    return render(request,"map.html")
+    return render(request,"passangerMap.html")
+
+@csrf_exempt
+def set_route(request):
+    source=request.POST.get("source")
+    destination=request.POST.get("destination")
+    return JsonResponse({"data":"Done"})
